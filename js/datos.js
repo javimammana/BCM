22909a75fe175fe0444fa48ac839e1895b30e136
javimammana <javiermammana.dev@gmail.com>
2025-10-14 11:32:06 -0300

    maguicumple 1er

---
+++ js/datos.js
@@ -0,0 +1,13 @@
+
+const fecha = new Date("11/08/2025 16:00");
+
+const horaEvento = "el 08/11/2025 a las 16:00Hs";
+const direccionLink = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d875.9694980260855!2d-64.15794246101758!3d-31.438113763241915!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9432a361fd47f995%3A0x8af43b76c78caea7!2sParroquia%20San%20Pablo%20Apostol!5e0!3m2!1ses-419!2sar!4v1759839528422!5m2!1ses-419!2sar";
+const direccionLink2 = "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d404.2046234014457!2d-64.15109717507686!3d-31.435606588631984!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9432a33445882441%3A0x779b566325f90fff!2sLa%20Casita%20M%C3%A1gica%20Fiestas%20Infantiles!5e0!3m2!1ses-419!2sar!4v1759839455885!5m2!1ses-419!2sar";
+const coordenadas = "Asturias 1935, X5014BQE X5014BQE, Córdoba";
+const coordenadas2 = "Asturias 2712, X5014 Córdoba";
+const formAsistencia = "https://wa.me/5493513283382";
+const formCanciones = "";
+const fechaGoogle = "";
+
+
